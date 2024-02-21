7e5bb8f529daaf95e3ca220afe7304afff92a9d0
geros10 <159140896+geros10@users.noreply.github.com>
2024-02-21 12:59:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const searchContainer = document.getElementById("searchContainer");
+const searchBar = document.getElementById("search-bar");
+const toggleBtn = document.querySelector('.toggle_btn');
+const linksContainer = document.querySelector('.navbar .links');
+
+searchBar.addEventListener("focus", function () {
+    searchContainer.style.width = "300px";
+});
+
+searchBar.addEventListener("blur", function () {
+    if (this.value === "") {
+        searchContainer.style.width = "200px";
+    }
+});
+
+toggleBtn.addEventListener('click', function() {
+    linksContainer.classList.toggle('show');
+});
